01ba1e9fd7cb5baf4467508aeb9e1d588d512a1d
Yousif Salah <a0b.oamer@gmail.com>
2026-07-15 16:39:46 +0300

    Extract shared Verify helper for configuration specs

--- tests/Fallout.Common.Specs/CI/AzurePipelinesJobSpecs.cs
+++ tests/Fallout.Common.Specs/CI/AzurePipelinesJobSpecs.cs
@@ -2,28 +2,15 @@
 using System.Threading.Tasks;
 using Fallout.Common.CI.AzurePipelines;
 using Fallout.Common.CI.AzurePipelines.Configuration;
-using Fallout.Common.Utilities;
-using VerifyXunit;
 using Xunit;
 
 namespace Fallout.Common.Specs.CI;
 
 public class AzurePipelinesJobSpecs
 {
-    private static Task Verify(AzurePipelinesJob job)
-    {
-        var stream = new MemoryStream();
-        var writer = new StreamWriter(stream, leaveOpen: true);
-        job.Write(new CustomFileWriter(writer, indentationFactor: 2, commentPrefix: "#"));
-        writer.Flush();
-
-        stream.Seek(offset: 0, SeekOrigin.Begin);
-        return Verifier.Verify(new StreamReader(stream).ReadToEnd());
-    }
-
     [Fact]
     public Task DisplayName_with_an_apostrophe_is_yaml_escaped()
-        => Verify(new AzurePipelinesJob
+        => ConfigurationEntityVerifier.Verify(new AzurePipelinesJob
                   {
                       Name = "Compile",
                       DisplayName = "Bob's Job",
@@ -33,7 +20,7 @@ public class AzurePipelinesJobSpecs
 
     [Fact]
     public Task VmImage_is_quoted_correctly_without_double_wrapping_quirk()
-        => Verify(new AzurePipelinesJob
+        => ConfigurationEntityVerifier.Verify(new AzurePipelinesJob
                   {
                       Name = "Compile",
                       DisplayName = "Compile Job",
---
+++ tests/Fallout.Common.Specs/CI/ConfigurationEntityVerifier.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Fallout.Common.CI;
+using Fallout.Common.Utilities;
+using VerifyXunit;
+
+namespace Fallout.Common.Specs.CI;
+
+public static class ConfigurationEntityVerifier
+{
+    public static Task Verify(ConfigurationEntity entity)
+    {
+        var stream = new MemoryStream();
+        var writer = new StreamWriter(stream, leaveOpen: true);
+        entity.Write(new CustomFileWriter(writer, indentationFactor: 2, commentPrefix: "#"));
+        writer.Flush();
+
+        stream.Seek(offset: 0, SeekOrigin.Begin);
+        return Verifier.Verify(new StreamReader(stream).ReadToEnd());
+    }
+}
